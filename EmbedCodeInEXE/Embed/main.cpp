c76284445b9a8288801b23f2d8c6ad1aad8b9c37
Alon Regev <alonr3704@gmail.com>
2022-09-10 18:42:11 +0300

    finding empty sections in code

--- EmbedCodeInEXE/Embed/main.cpp
+++ EmbedCodeInEXE/Embed/main.cpp
@@ -5,46 +5,63 @@
 #include <iostream>
 #include <string>
 
-void setFileEntryPoint(const char* szFilename, long newEntryPoint);
+#define RET_INSTRUCTION 0xC3
+
+void analyzeEXE(const char* exePath);
 
 int main()
 {
-    int x = 0;
     std::string exePath;
     printf("Enter executable name: ");
     std::getline(std::cin, exePath);
-    printf("Enter new entry point: ");
-    scanf("%x", &x);
-    setFileEntryPoint(exePath.c_str(), x);
+    analyzeEXE(exePath.c_str());
 }
 
-void setFileEntryPoint(const char* szFilename, long newEntryPoint)
+void analyzeEXE(const char* exePath)
 {
-    IMAGE_DOS_HEADER* pHead;
-    pHead = new IMAGE_DOS_HEADER;
-    FILE* file = fopen(szFilename, "r+b");
+    IMAGE_DOS_HEADER dosHeader;
+    FILE* file = fopen(exePath, "r+b");
     if (!file)
     {
         printf("Can't open file :(\n");
         return;
     }
-    fread(pHead, sizeof(IMAGE_DOS_HEADER), 1, file);
-    if (pHead->e_magic != IMAGE_DOS_SIGNATURE)
+    fread(&dosHeader, sizeof(IMAGE_DOS_HEADER), 1, file);
+    if (dosHeader.e_magic != IMAGE_DOS_SIGNATURE)
     {
         printf("Invalid magic :(\n");
         return;
     }
-    long peHeader = pHead->e_lfanew;
-    long peOptHeader = peHeader + 4 + sizeof(IMAGE_FILE_HEADER);
-    fseek(file, peOptHeader, SEEK_SET);
-    IMAGE_OPTIONAL_HEADER* pOpt;
-    pOpt = new IMAGE_OPTIONAL_HEADER;
-    fread(pOpt, sizeof(IMAGE_OPTIONAL_HEADER), 1, file);
-    printf("Entry point: %x\n", pOpt->AddressOfEntryPoint);
-    pOpt->AddressOfEntryPoint = newEntryPoint;
-    fseek(file, peOptHeader, SEEK_SET);
-    fwrite(pOpt, sizeof(IMAGE_OPTIONAL_HEADER), 1, file);
+    long peHeaderAddr = dosHeader.e_lfanew;
+    long optHeaderAddr = peHeaderAddr + 4 + sizeof(IMAGE_FILE_HEADER);
+    fseek(file, optHeaderAddr, SEEK_SET);
+    IMAGE_OPTIONAL_HEADER optHeader;
+    fread(&optHeader, sizeof(IMAGE_OPTIONAL_HEADER), 1, file);
+
+    // read code section and find unused sections
+    // best section is ret (C3) followed by many zeroes
+    int size = 0;
+    fseek(file, optHeader.BaseOfCode, SEEK_SET);
+    for (int i = 0; i < optHeader.SizeOfCode; i++)
+    {
+        unsigned char b;
+        fread(&b, sizeof(unsigned char), 1, file);
+        if (b == 0)
+            size++;
+        else
+        {
+            if (size > 16)
+            {
+                printf("size: %d\n", size);
+            }
+            size = 0;
+        }
+    }
+
+    // rewrite updated opt header
+    /*
+    fseek(file, optHeaderAddr, SEEK_SET);
+    fwrite(&optHeader, sizeof(IMAGE_OPTIONAL_HEADER), 1, file);
+    */
     fclose(file);
-    delete pOpt;
-    delete pHead;
 }
