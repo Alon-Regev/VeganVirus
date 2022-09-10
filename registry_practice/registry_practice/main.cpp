df53940c8ac3d2e6f674326a9917cb9690cc9575
Alon Regev <alonr3704@gmail.com>
2022-09-10 11:53:50 +0300

    fixed registry code

--- registry_practice/registry_practice/main.cpp
+++ registry_practice/registry_practice/main.cpp
@@ -1,12 +1,26 @@
 #include <windows.h>
-#define ICON_SIZE_REGISTRY_PATH  "Software\Microsoft\Windows\Shell\Bags\1\Desktop\IconSize"
-#include <iostream>
-#include<stdio.h>
+#include <stdio.h>
+
+#define ICON_SIZE_REGISTRY_PATH  "Software\\Microsoft\\Windows\\Shell\\Bags\\1\\Desktop"
+
 void main()
 {
-	DWORD iconSize = 256;
-	LSTATUS reg_value = RegSetValueExA(HKEY_CURRENT_USER, ICON_SIZE_REGISTRY_PATH, 0, REG_DWORD, (LPBYTE)&iconSize, sizeof(DWORD));
-	
+	DWORD iconSize = 32;
+    HKEY hkey = NULL;
+    LSTATUS ret = RegOpenKeyExA(HKEY_CURRENT_USER, ICON_SIZE_REGISTRY_PATH, 0, KEY_SET_VALUE, &hkey);
+    if (ret != ERROR_SUCCESS)
+    {
+        printf("Can't open registry key (Error %d)\n", ret);
+        return;
+    }
+	ret = RegSetValueExA(hkey, "IconSize", 0, REG_DWORD, (LPBYTE)&iconSize, sizeof(DWORD));
+    RegCloseKey(hkey);
+    if (ret != ERROR_SUCCESS)
+	{
+        printf("Can't change registry value (Error %d)\n", ret);
+		return;
+	}
 
+    system("tskill explorer");
 }
 	
