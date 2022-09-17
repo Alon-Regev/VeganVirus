49e12ae25864cf986e0cd15e023b1d0bd8af8617
arielsokolsky <arielsoko123@gmail.com>
2022-09-17 13:55:52 +0300

    change volume

--- sound/sound.cpp
+++ sound/sound.cpp
@@ -2,32 +2,80 @@
 #include "mmsystem.h"
 #include <Mmsystem.h>
 #include <mciapi.h>
+#include <mmeapi.h>
+#include <mmdeviceapi.h>
+#include <endpointvolume.h>
+
 //these two headers are already included in the <Windows.h> header
 #pragma comment(lib, "Winmm.lib")
 
 
-
+bool ChangeVolume(double nVolume, bool bScalar);
 
 
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
 {
-    mciSendStringA("play C:\\ariel\\projects\\VeganVirus\\foodSong.WAV wait", NULL, 0, 0); // no open or alias used
+    ChangeVolume(20, 1);
+    mciSendStringA("play C:\\foodSong.wav wait", NULL, 0, 0);
 
-    MSG msg;
 
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
 
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-        
-    }
 
-    return (int) msg.wParam;
+    return 0;
 }
 
+
+
+bool ChangeVolume(double nVolume, bool bScalar)
+{
+
+    HRESULT hr = NULL;
+    bool decibels = false;
+    bool scalar = false;
+    double newVolume = nVolume / 100;
+
+    CoInitialize(NULL);
+    IMMDeviceEnumerator* deviceEnumerator = NULL;
+    hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_INPROC_SERVER,
+        __uuidof(IMMDeviceEnumerator), (LPVOID*)&deviceEnumerator);
+    IMMDevice* defaultDevice = NULL;
+
+    hr = deviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &defaultDevice);
+    deviceEnumerator->Release();
+    deviceEnumerator = NULL;
+
+    IAudioEndpointVolume* endpointVolume = NULL;
+    hr = defaultDevice->Activate(__uuidof(IAudioEndpointVolume),
+        CLSCTX_INPROC_SERVER, NULL, (LPVOID*)&endpointVolume);
+    defaultDevice->Release();
+    defaultDevice = NULL;
+
+    // -------------------------
+    float currentVolume = 0;
+    endpointVolume->GetMasterVolumeLevel(&currentVolume);
+    //printf("Current volume in dB is: %f\n", currentVolume);
+
+    hr = endpointVolume->GetMasterVolumeLevelScalar(&currentVolume);
+    //CString strCur=L"";
+    //strCur.Format(L"%f",currentVolume);
+    //AfxMessageBox(strCur);
+
+    // printf("Current volume as a scalar is: %f\n", currentVolume);
+    if (bScalar == false)
+    {
+        hr = endpointVolume->SetMasterVolumeLevel((float)newVolume, NULL);
+    }
+    else if (bScalar == true)
+    {
+        hr = endpointVolume->SetMasterVolumeLevelScalar((float)newVolume, NULL);
+    }
+    endpointVolume->Release();
+
+    CoUninitialize();
+
+    return FALSE;
+}
